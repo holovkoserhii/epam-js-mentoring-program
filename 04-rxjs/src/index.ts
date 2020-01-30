b5eb9d94706edc254c36f3372bf3c610169d7b66
holovs01 <serhii_holovko1@epam.com>
2020-01-30 21:39:43 +0200

    change according to comments

--- 04-rxjs/src/index.ts
+++ 04-rxjs/src/index.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 const URL_BASE = 'https://jsonplaceholder.typicode.com/todos/'
 const DEBOUNCE_TIME = 1000
 const ITEMS_AT_ONCE = 30
-const todoTitles = []
+let todoTitles = []
 
 const filterSection: HTMLElement = document.querySelector('.filter')
 filterSection.style.display = 'none';
@@ -37,12 +37,6 @@ const filterItems = evt => {
   const subject = new Subject<string>();
   subject.pipe(
     debounceTime(DEBOUNCE_TIME),
-    // Unfortunately, my debounceTime doesn't work properly. It waits for a DEBOUNCE_TIME after each event and then
-    // throws a value EACH time, in spite of there are more "fresh" events it should react to, forgetting the old ones.
-    // I tried putting more operators either before debounceTime or after it (in pipe).
-    // There are also a lot of stuff explaining how debounceTime works, but almost all of them are about
-    // pure Observables, not Subjects. I didn't succeed it transferring the same logic to Subjects.
-    //E.g. this example: https://stackblitz.com/edit/typescript-adheqt?file=index.ts&devtoolsheight=50
   )
     .subscribe({
       next: () => updateView(evt.target.value),
