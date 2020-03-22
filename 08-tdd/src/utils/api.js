adece47d540c9f5beb6c0d390b2cacc2c5a24e02
holovs01 <serhii_holovko1@epam.com>
2020-03-22 16:39:20 +0200

    add api tests

--- 08-tdd/src/utils/api.js
+++ 08-tdd/src/utils/api.js
@@ -3,12 +3,7 @@ import axios from "axios";
 const BASE_API = "http://localhost:3000/notes/";
 
 export const deleteItem = id => axios.delete(BASE_API + id);
-export const updateItem = (id, props) => {
-  console.log("here!!!!111");
-  console.log(id);
-  console.log(props);
-  return axios.patch(BASE_API + id, props);
-};
+export const updateItem = (id, props) => axios.patch(BASE_API + id, props);
 export const addItem = item => axios.post(BASE_API, item);
 export const getItems = () => axios.get(`${BASE_API}?isArchived=false`);
 export const getArchivedItems = () => axios.get(`${BASE_API}?isArchived=true`);
